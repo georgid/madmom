a44e846f8fa3d5e7a6f3d92c08f79d43d240e3a1
georgid <georgi.dzhambazov@upf.edu>
2017-07-12 19:37:04 +0200

    mc

---
+++ madmom/Params.py
@@ -0,0 +1,21 @@
+'''
+Created on Apr 11, 2017
+
+@author: joro
+'''
+
+
+
+
+
+
+
+USUL_NUM_BEATS = {}
+USUL_NUM_BEATS['duyek'] =8
+USUL_NUM_BEATS['aksak'] =9
+USUL_NUM_BEATS['curcuna'] =10
+USUL_NUM_BEATS['kapali_curcuna'] =10
+USUL_NUM_BEATS['34'] =3 # wprkarpund to avoid ignoring code for tracking usul for western music
+USUL_NUM_BEATS['44'] =4
+
+WITH_GMM_PATTERN_TRACKER = 0 # mimic the behaviour of  GMMPatternTracker
